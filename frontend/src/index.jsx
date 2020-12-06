2d448014e41935a4cd742effa356448f74fccc6c
Rodrigo Garcia <rodrigo.mellogarcia@bridge.ufsc.br>
2020-12-06 18:05:06 -0300

    resolvidos conflitos

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App.jsx";
-=======
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
->>>>>>> base da pag principal
 
 const elem = document.createElement("div");
 document.body.appendChild(elem);
@@ -16,11 +9,6 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-<<<<<<< HEAD
-  elem
-);
-=======
   document.getElementById('root')
 )
 
->>>>>>> base da pag principal
