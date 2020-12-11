0b8cb548858e4dec52e15a887c5ba0b91eefe6c9
Rodrigo Garcia <rodrigo.mellogarcia@bridge.ufsc.br>
2020-12-11 17:44:25 -0300

    recebendo lista de propriedades

---
+++ frontend/src/components/PropertiesList/PropertiesList.jsx
@@ -0,0 +1,26 @@
+import "./PropertiesList.css"
+import PropertyBox from "./PropertyItem/PropertyBox.jsx"
+import React from "react"
+
+export default function PropertiesList(props) {
+  const { properties } = props
+
+  const convertToListItemObject = (item) => {
+    console.log(item)
+    return {
+      titulo:item.titulo,
+      endereco:item.endereco,
+      dormitorios:item.nr_dormitorios,
+      banheiros:item.nr_banheiros,
+      vagas:item.nr_vagas_garagem,
+      valor:item.valor,
+      situacao:item.situacao,
+    }
+  }
+
+  return (
+    <div className="post-list">
+      {properties ? properties.map((item, index) => <PropertyBox data={convertToListItemObject(item)} key={index}/>) : "Nenhum imóvel cadastrado"}
+    </div>
+  )
+}
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,8 @@
 import "./Home.css"
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Header from "../../components/header/Header.jsx"
 import { getProperties } from "../../model/requests"
+import PropertiesList from "../../components/PropertiesList/PropertiesList.jsx"
 
 export default function Home(props) {
   const [filtroData, setFiltroData] = useState({
@@ -12,25 +13,35 @@ export default function Home(props) {
     banheiros: "",
     garagem: "",
   })
-  
+
+  const [properties, setProperties] = useState()
+  useEffect(async () => {
+    const response = await getProperties(filtroData)
+    const listProperties = [...response]
+    setProperties(listProperties)
+
+    console.log(listProperties)
+  }, [])
+
   const handleInputChange = (event) => {
     const { name, value } = event.target
     setFiltroData({ ...filtroData, [name]: value })
   }
 
-  console.log(filtroData)
-
   const handleSubmit = async (event) => {
     event.preventDefault()
     const response = await getProperties(filtroData)
-    console.log(response)
+    const listProperties = [...response]
+    setProperties(listProperties)
+
+    console.log(listProperties)
   }
 
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <div className="Sidebar">
-        <form onSubmit={handleSubmit}> 
+        <form onSubmit={handleSubmit}>
           <div className="FiltroParent" >
             <h3>Filtros</h3>
             <p className="filtro-title">Preço do Aluguel</p>
@@ -46,56 +57,61 @@ export default function Home(props) {
 
             <p className="filtro-title">Tipo de Imóvel</p>
             <div className="FiltroRadio">
-              <input type="radio" value="" id="tipo-any" name="tipoImovel" onChange={handleInputChange}/>
-              <label htmlFor="tipo-any">Qualquer</label><br/>
-              <input type="radio" value="casa" id="tipo-casa" name="tipoImovel" onChange={handleInputChange}/>
-              <label htmlFor="tipo-casa">Casa</label><br/>
-              <input type="radio" value="apartamento" id="tipo-apartamento" name="tipoImovel" onChange={handleInputChange}/>
-              <label htmlFor="tipo-apartamento">Apartamento</label><br/>
+              <input type="radio" value="" id="tipo-any" name="tipoImovel" onChange={handleInputChange} />
+              <label htmlFor="tipo-any">Qualquer</label><br />
+              <input type="radio" value="casa" id="tipo-casa" name="tipoImovel" onChange={handleInputChange} />
+              <label htmlFor="tipo-casa">Casa</label><br />
+              <input type="radio" value="apartamento" id="tipo-apartamento" name="tipoImovel" onChange={handleInputChange} />
+              <label htmlFor="tipo-apartamento">Apartamento</label><br />
             </div>
 
             <p className="filtro-title">Quartos</p>
             <div className="FiltroRadio">
-              <input type="radio" value="" id="quartos-any" name="quartos" onChange={handleInputChange}/>
-              <label htmlFor="quartos-any">Qualquer</label><br/>
-              <input type="radio" value="1" id="quartos-1" name="quartos" onChange={handleInputChange}/>
-              <label htmlFor="quartos-1">1 quarto</label><br/>
-              <input type="radio" value="2" id="quartos-2" name="quartos" onChange={handleInputChange}/>
-              <label htmlFor="quartos-2">2 quartos</label><br/>
-              <input type="radio" value="3" id="quartos-3+" name="quartos" onChange={handleInputChange}/>
-              <label htmlFor="quartos-3+">3+ quartos</label><br/>
+              <input type="radio" value="" id="quartos-any" name="quartos" onChange={handleInputChange} />
+              <label htmlFor="quartos-any">Qualquer</label><br />
+              <input type="radio" value="1" id="quartos-1" name="quartos" onChange={handleInputChange} />
+              <label htmlFor="quartos-1">1 quarto</label><br />
+              <input type="radio" value="2" id="quartos-2" name="quartos" onChange={handleInputChange} />
+              <label htmlFor="quartos-2">2 quartos</label><br />
+              <input type="radio" value="3" id="quartos-3+" name="quartos" onChange={handleInputChange} />
+              <label htmlFor="quartos-3+">3+ quartos</label><br />
             </div>
 
             <p className="filtro-title">Banheiros</p>
             <div className="FiltroRadio">
-              <input type="radio" value="" id="banheiros-any" name="banheiros" onChange={handleInputChange}/>
-              <label htmlFor="banheiros-any">Qualquer</label><br/>
-              <input type="radio" value="1" id="banheiros-1" name="banheiros" onChange={handleInputChange}/>
-              <label htmlFor="banheiros-1">1 banheiro</label><br/>
-              <input type="radio" value="2" id="banheiros-2" name="banheiros" onChange={handleInputChange}/>
-              <label htmlFor="banheiros-2">2 banheiros</label><br/>
-              <input type="radio" value="3" id="banheiros-3+" name="banheiros" onChange={handleInputChange}/>
-              <label htmlFor="banheiros-3+">3+ banheiros</label><br/>
+              <input type="radio" value="" id="banheiros-any" name="banheiros" onChange={handleInputChange} />
+              <label htmlFor="banheiros-any">Qualquer</label><br />
+              <input type="radio" value="1" id="banheiros-1" name="banheiros" onChange={handleInputChange} />
+              <label htmlFor="banheiros-1">1 banheiro</label><br />
+              <input type="radio" value="2" id="banheiros-2" name="banheiros" onChange={handleInputChange} />
+              <label htmlFor="banheiros-2">2 banheiros</label><br />
+              <input type="radio" value="3" id="banheiros-3+" name="banheiros" onChange={handleInputChange} />
+              <label htmlFor="banheiros-3+">3+ banheiros</label><br />
             </div>
 
             <p className="filtro-title">Garagem</p>
             <div className="FiltroRadio">
-              <input type="radio" value="" id="garagem-any" name="garagem" onChange={handleInputChange}/>
-              <label htmlFor="garagem-any">Qualquer</label><br/>
-              <input type="radio" value="0" id="garagem-0" name="garagem" onChange={handleInputChange}/>
-              <label htmlFor="garagem-0">Sem vagas</label><br/>
-              <input type="radio" value="1" id="garagem-1" name="garagem" onChange={handleInputChange}/>
-              <label htmlFor="garagem-1">1 vaga</label><br/>
-              <input type="radio" value="2" id="garagem-2+" name="garagem" onChange={handleInputChange}/>
-              <label htmlFor="garagem-2+">2+ vagas</label><br/>
-              
+              <input type="radio" value="" id="garagem-any" name="garagem" onChange={handleInputChange} />
+              <label htmlFor="garagem-any">Qualquer</label><br />
+              <input type="radio" value="0" id="garagem-0" name="garagem" onChange={handleInputChange} />
+              <label htmlFor="garagem-0">Sem vagas</label><br />
+              <input type="radio" value="1" id="garagem-1" name="garagem" onChange={handleInputChange} />
+              <label htmlFor="garagem-1">1 vaga</label><br />
+              <input type="radio" value="2" id="garagem-2+" name="garagem" onChange={handleInputChange} />
+              <label htmlFor="garagem-2+">2+ vagas</label><br />
+
             </div>
-            
+
           </div>
           {/* <button className="filtro-button" type="submit" value="Aplicar Filtros"/> */}
           <button className="filtro-button" type="submit">Aplicar Filtros</button>
         </form>
       </div>
+      <div className="properties-parent">
+        <PropertiesList properties={properties}/>
+        {/* <div className="Teste"/> */}
+      </div>
+
     </div >
   )
 }
